76554c2f5cf2f1aedc2ec56b77b39059f5d50eeb
3t14 <michiru@r4hs.com>
2013-04-24 00:23:15 +0900

    Add Slider (SeekBar) component.

--- eclipse-project/Lesson01/src/com/dev_training/android/lesson01/MainActivity.java
+++ eclipse-project/Lesson01/src/com/dev_training/android/lesson01/MainActivity.java
@@ -3,9 +3,12 @@ package com.dev_training.android.lesson01;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,10 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		Toast.makeText(this, "Hello Android with Toast", Toast.LENGTH_LONG).show();
+		
+		
+		findViewById(R.id.seekBar1).setOnClickListener(this);
+		
 	}
 
 	@Override
@@ -20,6 +27,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		SeekBar seekBar1 = (SeekBar)findViewById(R.id.seekBar1);
+		
+		Toast.makeText(this, "seekBar1 value = "+seekBar1.getProgress(), Toast.LENGTH_LONG).show();
 	}
 
 }
