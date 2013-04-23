fec4c4ee98944e2a18757b2b3ca63cbc1e2bc187
3t14 <michiru@r4hs.com>
2013-04-23 19:02:39 +0900

    Add an onClickListener implementation method.

--- eclipse-project/Lesson01/src/com/dev_training/android/lesson01/MainActivity.java
+++ eclipse-project/Lesson01/src/com/dev_training/android/lesson01/MainActivity.java
@@ -3,9 +3,11 @@ package com.dev_training.android.lesson01;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		Toast.makeText(this, "You've just clicked the button.", Toast.LENGTH_LONG).show();
+		
 	}
 
 }
